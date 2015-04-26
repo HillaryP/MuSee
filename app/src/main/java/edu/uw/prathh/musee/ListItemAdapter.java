e4181b8a518dd4fb8ba7c6849f1af3152f283a42
Lily Dwyer <lmdwyer@uw.edu>
2015-04-26 13:33:27 -0700

    Adding custom font to menu screen, changing back button

--- app/src/main/java/edu/uw/prathh/musee/ListItemAdapter.java
+++ app/src/main/java/edu/uw/prathh/musee/ListItemAdapter.java
@@ -16,6 +16,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.graphics.Typeface;
+import android.content.Context;
 
 import edu.uw.prathh.musee.camera.CameraActivity;
 import edu.uw.prathh.musee.donate.DonateActivity;
@@ -48,6 +50,9 @@ public class ListItemAdapter extends ArrayAdapter {
         TextView labelView = (TextView) convertView.findViewById(R.id.label);
         labelView.setText(label);
         labelView.setTextColor(Color.parseColor("#FF5337"));
+        Typeface custom_font = Typeface.createFromAsset(context.getAssets(), "fonts/Avenir.ttc");
+        labelView.setTypeface(custom_font);
+
         final Intent goTo = getGoTo(label);
         labelView.setOnClickListener(new View.OnClickListener() {
             @Override
