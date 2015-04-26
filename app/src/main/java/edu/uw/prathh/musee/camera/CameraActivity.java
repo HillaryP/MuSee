09e156c592ef4ecbcd17453e79e314108da34718
Hillary Prather <prathh@uw.edu>
2015-04-25 19:07:28 -0700

    Connected to Wikitude SDK, camera will show up

--- app/src/main/java/edu/uw/prathh/musee/camera/CameraActivity.java
+++ app/src/main/java/edu/uw/prathh/musee/camera/CameraActivity.java
@@ -2,22 +2,36 @@ package edu.uw.prathh.musee.camera;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+
+import com.wikitude.architect.ArchitectView;
 
 import edu.uw.prathh.musee.MenuActivity;
 import edu.uw.prathh.musee.R;
 
 
 public class CameraActivity extends Activity {
+    public final String license = "XcZk4IBkd5sD6CYfdtXjmum1pQPDB9JZ4w47uEDEPM4Lm+D2Ljn96qndDAATnKRsAqQr9De0/6v0Nm385cAjCGMvk71cg0of4FO9OamGtjwnCiYG0Q2sPT0cYq/y2rXmgETeHcXmsHb63k05QRvS3Xt62QsaknmjwANLC/oQshNTYWx0ZWRfX+9PmWk5kTsqStwQ3lKgmaVMo1phjuzjaAItHfmHdDaNVr6IdEqrqLsRVxgpuUlkPX0bohm0fh89mpB+5tYZ+ycykAf4S9k7wL7Io5r7lX5o8glXj5vn64KT3W+I9f2NpvjRvjQ5MYBqJkiN00oZMBCfA0ExVUDbOqiCwjeWkoYGKH2UftYF/CJgKzFoOUpBdOuR3WLaYvATUuyowxtSKs/BoDDh9p7TxHvNgNILEayhzGtgC6Ux9uKZqocStgQSn82mhHTR6fgSpSbSfoAj3gbweCn9Vn70lzXsuo7zeZOTXxgALTAoKiXimG4iVJiXJwm0N7O5pMPwI/FMfEnot+wIBdcXJ3Tt7eV3d4mpar2exfxLG5AxZlVDWt7hiio4cY5kVS+ZcU/psClbgQgHEJPbC7TYmXvFVos3J9RzZ8xhlRYxutQDZtXziqfiILTsoxRhHJUavuj8sY1uKxsqu7XKMne8dfD/2g==";
+    private ArchitectView architectView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        this.architectView = (ArchitectView) findViewById(R.id.architect_view);
+        final ArchitectView.ArchitectConfig config = new ArchitectView.ArchitectConfig(license);
+        try {
+            this.architectView.onCreate(config);
+        } catch (Exception e) {
+            Log.e("CameraActivity", "ArchitectView could not be created: " + e.getMessage());
+        }
 
         ImageButton menuButton = (ImageButton) findViewById(R.id.menu_button);
         menuButton.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +43,37 @@ public class CameraActivity extends Activity {
         });
     }
 
+    @Override
+    public void onPostCreate(Bundle bundleSavedInstance) {
+        super.onPostCreate(bundleSavedInstance);
+        if (this.architectView != null) {
+            this.architectView.onPostCreate();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (this.architectView != null) {
+            this.architectView.onPause();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (this.architectView != null) {
+            this.architectView.onResume();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (this.architectView != null) {
+            this.architectView.onDestroy();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
