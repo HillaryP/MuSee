a44de5fc507c979f0c5b36d0c39ffb910ede89bf
Lily Dwyer <lmdwyer@uw.edu>
2015-04-26 12:16:59 -0700

    Making menu button clickable

--- app/src/main/java/edu/uw/prathh/musee/exhibits/ExhibitActivity.java
+++ app/src/main/java/edu/uw/prathh/musee/exhibits/ExhibitActivity.java
@@ -1,11 +1,15 @@
 package edu.uw.prathh.musee.exhibits;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import edu.uw.prathh.musee.MenuActivity;
 import edu.uw.prathh.musee.R;
 
 public class ExhibitActivity extends Activity {
@@ -16,6 +20,15 @@ public class ExhibitActivity extends Activity {
         setContentView(R.layout.activity_exhibit);
         TextView title = (TextView) findViewById(R.id.header).findViewById(R.id.title);
         title.setText("Exhibits");
+
+        ImageButton menuButton = (ImageButton) findViewById(R.id.menu_button);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ExhibitActivity.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
