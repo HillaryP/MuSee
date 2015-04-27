5e9222fd7119368c3328ebab1d312f2dc6d9409c
Hillary Prather <prathh@uw.edu>
2015-04-26 23:03:52 -0700

    Added Parse capability

--- app/src/main/java/edu/uw/prathh/musee/MuSeeApp.java
+++ app/src/main/java/edu/uw/prathh/musee/MuSeeApp.java
@@ -6,6 +6,9 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 /**
  * Created by hillaryprather on 3/7/15.
  */
@@ -17,6 +20,10 @@ public class MuSeeApp extends Application {
     public void onCreate() {
         super.onCreate();
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "0gdTYz0qLwmWSo1X1R7f0BySH5RUpw7X2gkzJPRC", "SVheNhubmxUkEprZ67RObXf98rdU7KRKSN7WL61y");
+
         Log.i("MuSeeApp", "Application created");
     }
 
