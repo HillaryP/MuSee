acd42dab0fb490d867869d12b0694f51c39033a2
Oscar <wong.s.oscar@gmail.com>
2015-05-21 19:25:00 -0700

    Started working on swipe functionality

---
+++ app/src/main/java/edu/uw/prathh/musee/TutorialSwipe.java
@@ -0,0 +1,16 @@
+package edu.uw.prathh.musee;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+/**
+ * Created by owong on 5/21/15.
+ */
+
+public class TutorialSwipe extends FragmentActivity{
+
+    static final int NUMBER_OF_PAGES = 5;
+
+    
+}
