b78f858a530c19b3af101d4108c53ab4d226a074
Oscar <wong.s.oscar@gmail.com>
2015-06-04 10:50:46 -0700

    Fixed the padding in the favorites page

--- app/src/main/java/edu/uw/prathh/musee/info/FavoritesActivity.java
+++ app/src/main/java/edu/uw/prathh/musee/info/FavoritesActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -64,10 +65,10 @@ public class FavoritesActivity extends FragmentActivity {
             linearLayout.setPadding(0, 400, 0, 0);
             TextView text = new TextView(this);
             text.setText("It looks like there's nothing here! Here's how to add a favorite:");
-            text.setPadding(50, 50, 15 , 5);
             text.setTextSize(20);
+            text.setGravity(Gravity.CENTER_HORIZONTAL);
             text.setLayoutParams(new ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT));
             linearLayout.addView(text);
             ImageView image = new ImageView(this);
