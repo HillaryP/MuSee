0be0837d3e7d554920ac1dbb983418601906cbc8
Oscar <wong.s.oscar@gmail.com>
2015-06-04 10:28:46 -0700

    Made changes to favorites activity imagery

--- app/src/main/java/edu/uw/prathh/musee/info/FavoritesActivity.java
+++ app/src/main/java/edu/uw/prathh/musee/info/FavoritesActivity.java
@@ -61,7 +61,7 @@ public class FavoritesActivity extends FragmentActivity {
                     ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT));
             linearLayout.setOrientation(LinearLayout.VERTICAL);
-            linearLayout.setPadding(0, 300, 0, 0);
+            linearLayout.setPadding(0, 400, 0, 0);
             TextView text = new TextView(this);
             text.setText("It looks like there's nothing here! Here's how to add a favorite:");
             text.setPadding(50, 50, 15 , 5);
